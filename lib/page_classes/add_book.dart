77fc6cc1d508060096889861df75400f8a05e138
Yavuz <b21627251@cs.hacettepe.edu.tr>
2020-03-29 18:36:25 +0300

    login post user added

--- lib/page_classes/add_book.dart
+++ lib/page_classes/add_book.dart
@@ -1,35 +1,65 @@
 import 'package:flutter/material.dart';
 
-class AddBook extends StatefulWidget{
+class AddBook extends StatefulWidget {
   @override
   _AddBookState createState() => new _AddBookState();
 }
 
-class _AddBookState extends State<AddBook>{
+class _AddBookState extends State<AddBook> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Container(height: 20,child: Image.asset("images/logo_white/logo_white.png"),),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {
-                  print("shopping");
-                }),
-          ],
+        title: Container(
+          height: 20,
+          child: Image.asset("images/logo_white/logo_white.png"),
         ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                print("shopping");
+              }),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Kitabin Adi"
-                ),
-              ),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: TextFormField(
+                    
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Author"),
+                  )
+                  ),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Book Name"),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Categori"),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Description"),
+                  )),
+              Padding(
+                  padding: const EdgeInsets.only(top: 3.0, bottom: 3.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "Language"),
+                  )),
             ],
           ),
         ),
