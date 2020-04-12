e30c6c3f10410063cbdb9a061df0839b391559be
aosmankocaman <aosmankocaman@gmail.com>
2020-04-12 20:17:02 +0300

    main page fixleri

--- lib/views/main_page/main_page.dart
+++ lib/views/main_page/main_page.dart
@@ -15,13 +15,163 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  
-
+  int _index = 0;
+  List<Widget> _pages = [MainPage(), SearchPage(), NotificationsPage(), AccountProfilePage()];
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-       
+        bottomNavigationBar: BottomAppBar(
+          elevation: 7,
+          child: Container(
+            height: 50,
+            color: Color(0xffe65100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: _index == 0
+                              ? Icon(
+                                  Icons.home,
+                                  size: 25,
+                                  color: Colors.white,
+                                )
+                              : Icon(
+                                  Icons.home,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainPage()))
+                          },
+                        ),
+                        _index == 0
+                            ? Text(
+                                "Ana Sayfa",
+                                style: TextStyle(
+                                    fontSize: 10, fontWeight: FontWeight.bold),
+                              )
+                            : SizedBox(),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: _index == 1
+                              ? Icon(
+                                  Icons.search,
+                                  size: 25,
+                                  color: Colors.white,
+                                )
+                              : Icon(
+                                  Icons.search,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SearchPage()))
+                          },
+                        ),
+                        _index == 1
+                            ? Text(
+                                "Arama",
+                                style: TextStyle(
+                                    fontSize: 10, fontWeight: FontWeight.bold),
+                              )
+                            : SizedBox(),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: _index == 2
+                              ? Icon(
+                                  Icons.notifications,
+                                  size: 25,
+                                  color: Colors.white,
+                                )
+                              : Icon(
+                                  Icons.notifications_none,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NotificationsPage()))
+                          },
+                        ),
+                        _index == 2
+                            ? Text(
+                                "Bildirimler",
+                                style: TextStyle(
+                                    fontSize: 10, fontWeight: FontWeight.bold),
+                              )
+                            : SizedBox(),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: _index == 3
+                              ? Icon(
+                                  Icons.person,
+                                  size: 25,
+                                  color: Colors.white,
+                                )
+                              : Icon(
+                                  Icons.person_outline,
+                                  size: 30,
+                                  color: Colors.white,
+                                ),
+                          onTap: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => AccountProfilePage()))
+                          },
+                        ),
+                        _index == 3
+                            ? Text(
+                                "Profilim",
+                                style: TextStyle(
+                                    fontSize: 10, fontWeight: FontWeight.bold),
+                              )
+                            : SizedBox(),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
         appBar: AppBar(
           title: Image.asset("images/logo_white/logo_white.png"),
           actions: <Widget>[
@@ -33,12 +183,18 @@ class _MainPageState extends State<MainPage> {
           ],
         ),
         drawer: SahafDrawer(),
-        body:ListView(
+        body: ListView(
           children: <Widget>[
             HomePageCarousel(),
             BookCard(),
           ],
-        )
-        );
+        ));
+  }
+
+  _changeIndex(int i) {
+    _index = i;
+    setState(() {
+
+    });
   }
 }
